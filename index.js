123b392076fc35af0ea35049a9936744728741ad
Anubhav Misra <anubhav.misra@empeiriatech.com>
2017-09-11 18:13:18 +0530

    Working hello world

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-/*
-* HTTP Cloud Function.
-*
-* @param {Object} req Cloud Function request context.
-* @param {Object} res Cloud Function response context.
-*/
-exports.helloHttp = function helloHttp (req, res) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+var express = require('express');
+var stringify = require('json-stringify');
+
+var app = express();
+
+app.get('/api/weather', function(req, res) {
+  var response = 'This is a sample response from your webhook!'; //Default response from the webhook to show it's working
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response 
+  res.send(stringify({ "speech": response, "displayText": response
   //"speech" is the spoken version of the response, "displayText" is the visual version
   }));
-};
+});
+
+app.listen(process.env.PORT || 3001);
