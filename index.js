4309f2bfd1f3386a8d68c1083380c1465f2af727
Anubhav Misra <anubhav.misra@empeiriatech.com>
2017-09-11 19:10:22 +0530

    adding response with city and date

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var stringify = require('json-stringify');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 var app = express();
 
@@ -7,13 +12,8 @@ app.post('/api/weather', function(req, res) {
   // Get the city and date from the request
   console.log(req.body);
   var city = req.body.result.parameters['geo-city']; // city is a required param
-  // Get the date for the weather forecast (if present)
-  var date = '';
-  if (req.query.date) {
-    date = req.query.date;
-    console.log('Date: ' + date);
-  }
-  var response = 'Server says that weather for ' + city + ' on ' + date + ' is unknown.'; //Default response from the webhook to show it's working
+
+  var response = 'Server says that weather for ' + city + ' is unknown.'; //Default response from the webhook to show it's working
   
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
   res.send(stringify({ "speech": response, "displayText": response
