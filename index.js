1fdf77210a83a5577f5d0741f0447b584d3ef171
Anubhav Misra <anubhav.misra@empeiriatech.com>
2017-09-13 09:14:50 +0530

    Update

--- index.js
+++ index.js
@@ -22,4 +22,18 @@ app.post('/api/weather', function(req, res) {
   }));
 });
 
+app.post('/api/order', function(req, res) {
+  // Get the city and date from the request
+  console.log(req.body);
+  var product = req.body.result.parameters['product']; // product is a required param
+  //var brand = req.body.result.parameters['brand']; // brand is a required param
+
+  var response = 'I have added ' + product + ' to your basket(Not really).'; //Default response from the webhook to show it's working
+  
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(stringify({ "speech": response, "displayText": response
+  //"speech" is the spoken version of the response, "displayText" is the visual version
+  }));
+});
+
 app.listen(process.env.PORT || 3001);
