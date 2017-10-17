6b00b7d9a1b1141f899035ed87024e7bb256c444
Joris Borsboom <jorisborsboom@gmail.com>
2017-10-17 10:50:31 +0530

    Adding a call to MB search api to retrieve actual results

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var stringify = require('json-stringify');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 app.use(bodyParser.json());       // to support JSON-encoded bodies
@@ -59,6 +60,26 @@ function callWeatherApi (city, date) {
   });
 }
 
+function callSearch(query){
+  return new Promise((resolve, reject) => {
+    var options =  {
+        uri: 'http://milkbasket.com/products/search',
+        body: JSON.stringify({"search_text": query}),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    request(options, function (error, response) {
+      if (!error && response.statusCode == 200) {
+        resolve(JSON.parse(response.body));
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
 app.post('/api/weather', (req, res) => {
   // Get the city and date from the request
   let city = req.body.result.parameters['geo-city']; // city is a required param
@@ -87,25 +108,41 @@ app.post('/api/order', function(req, res) {
   var quantity = req.body.result.parameters['quantity'];
   //var brand = req.body.result.parameters['brand']; // brand is a required param
 
+  callSearch(product).then((output) => {
+    // console.log("output length " + output.data.length);
+    // console.log(output);
+    if(output.data.length > 1){
+      var response = 'I have found multiple products.';
 
-  if(product === "bread"){
-    var response = 'I have found multiple products.';
+      //"followupEvent" to send the user to the next step
+      var responseJson = stringify({ "speech": response, "displayText": response, "followupEvent": {
+          "name": "product_multiple_results",
+          "data": {
+             //"products": ["White cheap bread", "Fancy artisan bread", "Sourdough"]
+             "products":output.data
+          }
+        }});
+    } else if (output.data.length == 1){
+      //TODO: add this product to the basket
 
-    //"followupEvent" to send the user to the next step
-    var responseJson = stringify({ "speech": response, "displayText": response, "followupEvent": {
-        "name": "product_multiple_results",
-        "data": {
-           "products": ["White cheap bread", "Fancy artisan bread", "Sourdough"]
-        }
-      }});
-  } else {
-    //"speech" is the spoken version of the response, "displayText" is the visual version
-    var response = 'I have added ' + product + ' to your basket(Not really).'; //Default response from the webhook to show it's working
-    var responseJson = stringify({ "speech": response, "displayText": response});
-  }
+      //"speech" is the spoken version of the response, "displayText" is the visual version
+      var response = 'I have added \'' + output.data[0].nm + '\' to your basket(Not really).'; //Default response from the webhook to show it's working
+      var responseJson = stringify({ "speech": response, "displayText": response});
+    } else if (output.data.length < 1){
+      var response = 'I could not find any results for ' + product + '.'; //Default response from the webhook to show it's working
+      var responseJson = stringify({ "speech": response, "displayText": response});
+    }
 
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(responseJson);
+    res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+    res.send(responseJson);
+  }).catch((error) => {
+    console.log(error);
+    // If there is an error let the user know
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+  });
+
+
 });
 
 app.listen(process.env.PORT || 3001);
